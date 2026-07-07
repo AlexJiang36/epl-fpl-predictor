96fc9daf01807cea936c87e99eeb64be35c7d822
Alex Jiang <yue_jiang2019@outlook.com>
2026-07-07 14:08:46 -0700

    Day62.11: make current season configurable

--- backend/app/core/season.py
+++ backend/app/core/season.py
@@ -1,5 +1,9 @@
-CURRENT_SEASON = "2025_26"
+import os
+
+
+DEFAULT_SEASON = "2025_26"
+SEASON_ENV_VAR = "FPL_SEASON"
 
 
 def get_current_season() -> str:
-    return CURRENT_SEASON
+    return os.environ.get(SEASON_ENV_VAR, DEFAULT_SEASON)
